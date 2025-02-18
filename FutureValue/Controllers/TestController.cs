43d665c340d823a5b2fac9fe5bee3bebd822575c
axe53 <agitty84.ae@gmail.com>
2025-02-17 17:09:27 -0800

    added bootstrap and popperJS for responsive visual

---
+++ FutureValue/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FutureValue.Controllers
+{
+    public class TestController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult TestView()
+        {
+            ViewBag.email = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
